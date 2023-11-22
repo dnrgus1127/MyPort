02dea7e2fec355d745406e433fa7de3f39299f19
dnrgus1127 <dnrgus1127@naver.com>
2023-11-22 11:45:40 +0900

    FEAT (타이핑 컴포넌트) : 문자열 타이핑 컴포넌트 제작

---
+++ src/components/Common/EffectElement/TypingText.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components';
+
+interface TypingTextProps {
+    text: string;
+    tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "li";
+    delay?: number;
+    speed?: number;
+}
+
+const TypingTextCss = styled.h1`
+    
+`
+
+export default function TypingText({ tag = "h1", text ,delay = 0,speed=100 }: TypingTextProps) {
+    const [innerText, setInnerText] = useState<string>("");
+
+    useEffect(() => {
+        const initDelay = delay;
+        let speed = 100;
+        let length = 1;
+        let typingIntervalId: NodeJS.Timer;
+        // 시작 딜레이 타이머
+        let timerId = setTimeout(() => {
+            typingIntervalId = setInterval(() => {
+                setInnerText(text.substring(0, length));
+                length++;
+                if (length > text.length) {
+                    clearInterval(typingIntervalId);
+                }
+            },speed)
+            return () => {
+                clearInterval(typingIntervalId);
+            }
+        },initDelay)
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    },[text])
+  return (
+      <TypingTextCss as={tag}>{innerText}</TypingTextCss>
+  )
+}
