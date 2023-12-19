9690c560c5f00f6583023049348e4eb2f465f47b
dnrgus1127 <dnrgus1127@naver.com>
2023-12-19 17:35:54 +0900

    Style : 코드 포맷팅 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,96 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter,RouterProvider, ScrollRestoration } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import StacksPage from 'components/Stacks/StacksPage';
-import ProjectPage, {loader as projectLoader} from 'pages/ProjectPage';
-import ProjectSliderConatiner from 'components/Projects/ProjectSliderConatiner';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import BlogCategoryContents from "components/Blog/BlogMain/BlogCategoryContents";
+import BlogMainContents from "components/Blog/BlogMain/BlogMainContents";
+import BlogMainLayout from "components/Blog/BlogMain/BlogMainLayout";
+import ProjectSliderConatiner from "components/Projects/ProjectSliderConatiner";
+import StacksPage from "components/Stacks/StacksPage";
+import HomePage from "HomePage";
+import BlogPage, { loader as blogLoader } from "pages/BlogPage";
+import ErrorPage from "pages/ErrorPage";
+import PostPage, { loader as postLoader } from "pages/PostPage";
+import ProjectPage, { loader as projectLoader } from "pages/ProjectPage";
+import RootLayout from "pages/RootLayout";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from 'redux/store';
-import BlogPage, {loader as blogLoader} from 'pages/BlogPage';
-import BlogMainLayout from 'components/Blog/BlogMain/BlogMainLayout';
-import BlogMainContents from 'components/Blog/BlogMain/BlogMainContents';
-import BlogCategoryContents from 'components/Blog/BlogMain/BlogCategoryContents';
-import PostPage, {loader as postLoader} from 'pages/PostPage';
-import ErrorPage from 'pages/ErrorPage';
-import HomePage, {loader as homeLoader} from 'HomePage';
-import RootLayout from 'pages/RootLayout';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from "redux/store";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
-    element : <RootLayout/>,
-    children :[ {
-      path: "/",
-      element: <HomePage />,
-      loader : homeLoader(queryClient),
-      children: [
-         {
-          path: "project",
-          element: <ProjectPage />,
-          loader: projectLoader(queryClient),
-          children: [
-            {
-              path: "main",
-              element: <ProjectSliderConatiner />,
-            }
-          ]
-        },
-        {
-          path: "/Stack",
-          element: <StacksPage />,
-        },
-        
-      ]
-    },
-    {
-      path: "blog",
-      element: <BlogPage />,
-      errorElement: <ErrorPage/>,
-      loader: blogLoader(queryClient),
-      children: [
-        {
-          element: <BlogMainLayout />,
-          children: [
-            {
-              path: "main/",
-              element : <BlogMainContents/>
-            },
-            {
-              path: "main/:category",
-              element: <BlogCategoryContents />,
-              errorElement : <div></div>,
-            }
-          ]
-        },
-        {
-          path: "post/*",
-          element: <PostPage />,
-          loader: postLoader(queryClient),
-          
-          
-        }
-      ]
-    }
-    ]
-  }
-])
-
-
-
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+        // loader : homeLoader(queryClient),
+        children: [
+          {
+            path: "project",
+            errorElement: <ErrorPage />,
+            element: <ProjectPage />,
+            loader: projectLoader(queryClient),
+            children: [
+              {
+                path: "main",
+                element: <ProjectSliderConatiner />,
+              },
+            ],
+          },
+          {
+            path: "/Stack",
+            element: <StacksPage />,
+          },
+        ],
+      },
+      {
+        path: "blog",
+        errorElement: <ErrorPage />,
+        element: <BlogPage />,
+        loader: blogLoader(queryClient),
+        children: [
+          {
+            element: <BlogMainLayout />,
+            children: [
+              {
+                path: "main/",
+                element: <BlogMainContents />,
+              },
+              {
+                path: "main/:category",
+                element: <BlogCategoryContents />,
+                errorElement: <div></div>,
+              },
+            ],
+          },
+          {
+            path: "post/*",
+            element: <PostPage />,
+            loader: postLoader(queryClient),
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-        <Provider store={store} >
-              <RouterProvider router={router}/>
-        </Provider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
