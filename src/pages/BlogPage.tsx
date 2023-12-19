6ee2e1f9639106a0cac0d6b7adaa4debf0db163b
dnrgus1127 <dnrgus1127@naver.com>
2023-12-19 17:33:39 +0900

    Style : 코드 포맷팅 수정

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,25 +1,25 @@
-import { QueryClient } from '@tanstack/query-core'
-import { useQuery } from '@tanstack/react-query'
-import { Tree } from 'components/Common/Markdown/types/tree'
-import React, { useMemo } from 'react'
-import BlogLayout from 'components/Blog/BlogLayout'
-import BlogHeader from 'components/Blog/BlogHeader'
-import useLatestTimePost from 'components/Blog/hooks/useLatestTimePost'
-import { GITHUBAPIKEY } from 'apiKey'
-import { Outlet, json, useOutletContext } from 'react-router-dom'
-import { checkStatus } from 'utils/fetch/checkStatus'
-import { GITHUB403 } from 'constans/ErrorMessage'
+import { QueryClient } from "@tanstack/query-core";
+import { useQuery } from "@tanstack/react-query";
+import { Tree } from "components/Common/Markdown/types/tree";
+import React, { useMemo } from "react";
+import BlogLayout from "components/Blog/BlogLayout";
+import BlogHeader from "components/Blog/BlogHeader";
+import useLatestTimePost from "components/Blog/hooks/useLatestTimePost";
+import { GITHUBAPIKEY } from "apiKey";
+import { Outlet, json, useOutletContext } from "react-router-dom";
+import { checkStatus } from "utils/fetch/checkStatus";
+import { GITHUB403 } from "constans/ErrorMessage";
 
 const blogQuery = () => ({
   queryKey: ["git", "TIL", "tree"],
   queryFn: async () => {
     const res = await fetch(`https://api.github.com/repos/dnrgus1127/TIL/git/trees/main?recursive=10`, {
       headers: {
-        "Authorization" : GITHUBAPIKEY
-      }
+        Authorization: GITHUBAPIKEY,
+      },
     });
     if (res.status === 403) {
-      throw json({sorry:"Github API rate limit exceede", msg: GITHUB403}, { status: 403 });
+      throw json({ sorry: "Github API rate limit exceede", msg: GITHUB403 }, { status: 403 });
     }
 
     checkStatus(res.status);
@@ -27,46 +27,40 @@ const blogQuery = () => ({
     const data = await res.json();
     return data.tree;
   },
-  staleTime : 60 * 10 * 1000
-  
-})
+  staleTime: 60 * 10 * 1000,
+});
 
-export const loader = (queryClient : QueryClient) => async () => {
+export const loader = (queryClient: QueryClient) => async () => {
   const query = blogQuery();
 
-  return (
-    queryClient.getQueryData(query.queryKey) ?? (await queryClient.fetchQuery(query))
-  )
-}
+  return queryClient.getQueryData(query.queryKey) ?? (await queryClient.fetchQuery(query));
+};
 
 export default function BlogPage() {
-  
-  const { data,isSuccess } = useQuery<Array<Tree>>({ ...blogQuery() });
+  const { data, isSuccess } = useQuery<Array<Tree>>({ ...blogQuery() });
 
   const [topics, posts] = useMemo(() => {
-    if (!isSuccess) return [[],[]];
-    const topics = data.filter(item => item.type === "tree" && !item.path.includes("/"));
-    
-    const posts = data.filter(item => item.path.includes(".md"));
-    return [topics.map(topic => topic.path), posts];
-  }, [data,isSuccess])
-  
+    if (!isSuccess) return [[], []];
+    const topics = data.filter((item) => item.type === "tree" && !item.path.includes("/"));
+
+    const posts = data.filter((item) => item.path.includes(".md"));
+    return [topics.map((topic) => topic.path), posts];
+  }, [data, isSuccess]);
+
   const timeStampPosts = useLatestTimePost({ postList: posts });
   timeStampPosts.sort((a, b) => {
-    return (new Date(b.timeStamp!)).getTime() - (new Date(a.timeStamp!)).getTime()
-  })
-
-  
+    return new Date(b.timeStamp!).getTime() - new Date(a.timeStamp!).getTime();
+  });
 
   return (
-    <BlogLayout> 
+    <BlogLayout>
       <BlogHeader lastUpdate={timeStampPosts[0]} />
-      <Outlet context={{topics,timeStampPosts} satisfies ContextType}/>
+      <Outlet context={{ topics, timeStampPosts } satisfies ContextType} />
     </BlogLayout>
-  )
+  );
 }
 
-type ContextType = {topics : Array<string> , timeStampPosts : Array<Tree>};
+type ContextType = { topics: Array<string>; timeStampPosts: Array<Tree> };
 
 export function usePostTree() {
   return useOutletContext<ContextType>();
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,120 +1,125 @@
-import React, { ReactNode, useEffect,  useLayoutEffect,  useRef, useState} from 'react'
-import styled from 'styled-components'
-import About from '../components/Home/Sections/About'
-import Gnb from '../components/Home/Gnb'
-import PortFolio from '../components/Home/Sections/PortFolio'
-import Skills from '../components/Home/Sections/Skills'
-import Intro from '../components/Home/Sections/Intro'
-import Blog from 'components/Home/Sections/Blog'
-import Contact from 'components/Home/Sections/Contact'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { throttle } from 'lodash'
-import useTouchScroll from 'components/Home/hooks/useTouchScroll'
+import Blog from "components/Home/Sections/Blog";
+import Contact from "components/Home/Sections/Contact";
+import useSectionAnimation from "components/Home/Sections/hooks/useSectionAnimation";
+import useTouchScroll from "components/Home/hooks/useTouchScroll";
+import { throttle } from "lodash";
+import React, { ReactNode, useCallback, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Gnb from "../components/Home/Gnb";
+import About from "../components/Home/Sections/About";
+import Intro from "../components/Home/Sections/Intro";
+import PortFolio from "../components/Home/Sections/PortFolio";
+import Skills from "../components/Home/Sections/Skills";
 
 const HomeLayout = styled.div`
-    width: 100%;
-    height: calc(var(--vh) * 100);
-    overflow: hidden;
-    position: relative;
-`
+  width: 100%;
+  height: calc(var(--vh) * 100);
+  overflow: hidden;
+  position: relative;
+`;
 
 interface HomeScrollWrapperProps {
-    $sectionIndex: number;
+  $sectionIndex: number;
 }
 
 const HomeScrollWrapper = styled.div<HomeScrollWrapperProps>`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top : 0;
-    transform: translateY(${(props)=>props.$sectionIndex * 100 * -1}%);
-    left:0;
-`
-    
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  transform: translateY(${(props) => props.$sectionIndex * 100 * -1}%);
+  left: 0;
+`;
 
 export default function Home() {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { scrollWrapper: scrollWrapperRef, transitionEndHandler } = useTouchScroll({index : Number(location.hash.substring(1)), setIndex : (index: number) => navigate(`#${index}`)});
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { scrollWrapper: scrollWrapperRef, transitionEndHandler } = useTouchScroll({
+    index: Number(location.hash.substring(1)),
+    setIndex: (index: number) => navigate(`#${index}`),
+  });
 
   return (
-    <HomeLayout >
-          <HomeScrollWrapper ref={scrollWrapperRef} $sectionIndex={Number(location.hash.substring(1))} onTransitionEnd={transitionEndHandler}>
-              <ContentsSection contents={<Intro/>} flag= {0}/>
-              <ContentsSection contents={<About/>} flag= {1}/>
-              <ContentsSection contents={<Skills/>} flag= {2}/>
-              <ContentsSection contents={<PortFolio />} flag= {3}/>
-              <ContentsSection contents={<Blog/>} flag= {4}/>
-              <ContentsSection contents={<Contact />} flag= {5}/>
-          </HomeScrollWrapper>
-              <Gnb />
+    <HomeLayout>
+      <HomeScrollWrapper
+        ref={scrollWrapperRef}
+        $sectionIndex={Number(location.hash.substring(1))}
+        onTransitionEnd={transitionEndHandler}
+      >
+        <ContentsSection contents={<Intro />} flag={0} />
+        <ContentsSection contents={<About />} flag={1} />
+        <ContentsSection contents={<Skills />} flag={2} />
+        <ContentsSection contents={<PortFolio />} flag={3} />
+        <ContentsSection contents={<Blog />} flag={4} />
+        <ContentsSection contents={<Contact />} flag={5} />
+      </HomeScrollWrapper>
+      <Gnb />
     </HomeLayout>
-  )
+  );
 }
 
 const Section = styled.section`
-    width: 100%;
-    height : 100%;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
 
-    &::-webkit-scrollbar {
-        width: 4px;
-    }
-    &::-webkit-scrollbar-thumb {
-        height: 10%; /* 스크롤바의 길이 */
-        background: ${({theme})=>theme.pointColor}; /* 스크롤바의 색상 */
-        
-        border-radius: 4px;
-    }
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+  &::-webkit-scrollbar-thumb {
+    height: 10%; /* 스크롤바의 길이 */
+    background: ${({ theme }) => theme.pointColor}; /* 스크롤바의 색상 */
 
-    &::-webkit-scrollbar-track {
-    }
-`
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-track {
+  }
+`;
 const SectionWrapper = styled.div`
-    height: 100%;
-`
+  height: 100%;
+`;
 
-interface ContentsScrtionProps { 
-    contents: ReactNode;
-    isSetup?: boolean;
-    flag: number;
+interface ContentsScrtionProps {
+  contents: ReactNode;
+  isSetup?: boolean;
+  flag: number;
 }
 
 const max = 5;
 const min = 0;
 
-function ContentsSection({ contents,flag }: ContentsScrtionProps) {
-    const sectionRef = useRef<HTMLElement>(null);
-    const contentRef = useRef<HTMLDivElement>(null);
-    const location = useLocation();
-    const navigate = useNavigate();
+function ContentsSection({ contents, flag }: ContentsScrtionProps) {
+  const sectionRef = useRef<HTMLElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+  useSectionAnimation(flag);
 
-    useEffect(() => {
-        // Section 이동 이후 발생하는 추가적인 스크롤 이벤트에 대해서 처리 X    
-        if (!sectionRef.current || location.hash !== `#${flag}`) return;
-    
-        const section = sectionRef.current;
-        const handler = (e: WheelEvent) => {
-            let nextSection = Number(flag) + (e.deltaY < 0 ? -1 : 1);
-            if (nextSection >= min && nextSection <= max) {
-                navigate(`#${nextSection}`);
-            }
-        }
+  const wheelEventHandler = useCallback(
+    throttle((e: React.WheelEvent) => {
+      if (location.hash !== `#${flag}`) return;
+      if (Math.abs(e.deltaY) < window.innerHeight * 0.02) return;
+      let nextSection = Number(flag);
+      if (e.deltaY < 0) {
+        nextSection -= 1;
+      }
 
-        let throttleHandler = throttle(handler,1000);
+      if (e.deltaY > 0) {
+        nextSection += 1;
+      }
+      if (nextSection >= min && nextSection <= max) {
+        navigate(`#${nextSection}`);
+      }
+    }, 100),
+    [navigate, flag, location.hash]
+  );
 
-        section.addEventListener("wheel", throttleHandler);
-
-      
-        return () => {
-            section.removeEventListener("wheel", throttleHandler);
-        }
-
-    }, [location.hash,flag,navigate])
-    
-    
-    
-    
-    return <Section ref={sectionRef} className='scrollSection' >
-        <SectionWrapper ref={contentRef}>{contents}</SectionWrapper>
+  return (
+    <Section ref={sectionRef} onWheel={wheelEventHandler}>
+      <SectionWrapper ref={contentRef}>{contents}</SectionWrapper>
     </Section>
+  );
 }
