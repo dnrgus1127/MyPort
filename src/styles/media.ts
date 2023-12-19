1c48fe283e10f012d8b2378ccb8f952036902ff1
dnrgus1127 <dnrgus1127@naver.com>
2023-12-19 17:32:30 +0900

    Fix : 미디어 쿼리 픽셀 기준 수정
    767 => 768px

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -1,17 +1,13 @@
-export const mediaQuery = (maxWidth : number) => `
+export const mediaQuery = (maxWidth: number) => `
   @media (max-width: ${maxWidth}px)
 `;
 
-
-
 const media = {
-  xlarge : mediaQuery(1479), // 태블릿
+  xlarge: mediaQuery(1479), // 태블릿
   large: mediaQuery(1023), // 작은 태블릿
-  medium: mediaQuery(767), // 작은 태블릿
+  medium: mediaQuery(768), // 작은 태블릿
   small: mediaQuery(480), // 모바일 세로
   custom: mediaQuery,
 };
 
 export default media;
-
-
