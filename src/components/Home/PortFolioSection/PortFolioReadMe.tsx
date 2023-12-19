28f2ba05efef7a5faeac2586eb4877f89af6d9be
dnrgus1127 <dnrgus1127@naver.com>
2023-12-19 17:28:06 +0900

    FEAT (프로젝트) : 프로젝트 설명 화면 구성

---
+++ src/components/Home/PortFolioSection/PortFolioReadMe.tsx
@@ -0,0 +1,262 @@
+import MarkdownRender from "components/Common/Markdown/MarkdownRender";
+import { DirComponentCss, MarkdownStyled } from "components/Common/Markdown/MarkdownStyledComponent";
+import useReadme from "components/Projects/hooks/useReadme";
+import { PROJECT_INFOMATION } from "constans/ProjectData";
+import { getSkillColor } from "constans/SkillsData";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+import { AnimationComponent } from "styles/animation";
+import media from "styles/media";
+import { customScrollBar } from "styles/scrollBar";
+import { Repository, RepositoryConstant } from "types/Project";
+import useWheelStopPropagation from "../Sections/hooks/useWheelStopPropagation";
+import MarkdownDirTree from "components/Common/Markdown/MarkdownDirTree";
+import LoadingComponent from "components/Common/LoadingComponent";
+
+const PortFolioReadMeLayout = styled(AnimationComponent)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  transition: opacity 0s 1s ease-out;
+  opacity: 0;
+  display: flex;
+  ${customScrollBar};
+
+  ${(props) =>
+    props.$visible &&
+    css`
+      transition-delay: 0s;
+      opacity: 1;
+    `}
+
+  ${media.medium} {
+    margin: var(--header) 0;
+    flex-direction: column;
+    height: inherit;
+    overflow-y: scroll;
+  }
+`;
+
+const Section = styled(AnimationComponent)`
+  width: 50%;
+  height: 100%;
+  padding: var(--header);
+  ${media.medium} {
+    width: 100%;
+    height: auto;
+    padding: 3rem;
+  }
+`;
+
+const FirstSection = styled(Section)`
+  padding-right: calc(var(--header) / 2);
+  transition: transform 1s ease-out;
+  transform: translateX(-100%);
+  ${(props) =>
+    props.$visible &&
+    css`
+      transition-delay: 0.3s;
+      transform: translateX(0px);
+    `}
+
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 0.6em;
+    text-transform: uppercase;
+    text-align: center;
+  }
+  h2 {
+    font-size: 1.8rem;
+    margin: 0.7rem 0;
+    margin-top: 3rem;
+  }
+  h1,
+  h2 {
+    font-family: "Poppins";
+    font-weight: 600;
+  }
+
+  p {
+    font-family: "SUIT-Regular";
+    font-size: 1.5rem;
+    line-height: 1.7rem;
+
+    text-indent: 1rem;
+  }
+
+  .stack {
+    display: flex;
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+
+  .functions {
+    padding-left: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: 2rem;
+    li {
+      width: 50%;
+      margin: 1rem 0;
+      font-size: 1.6rem;
+      list-style-type: circle;
+      padding-left: 0.2rem;
+    }
+  }
+  .library {
+    margin-left: 1rem;
+    li {
+      list-style: "- ";
+      margin: 1rem 0;
+    }
+  }
+`;
+
+const ReadmeSection = styled(Section)`
+  overflow-y: scroll;
+  box-shadow: 0px 0px 15px 5px ${({ theme }) => theme.shadowColor};
+  background-color: ${({ theme }) => theme.bgColor3}aa;
+  position: relative;
+
+  transition: transform 1s ease-out;
+  transform: translateX(100%);
+  ${(props) =>
+    props.$visible &&
+    css`
+      transition-delay: 0.3s;
+      transform: translateX(0px);
+    `}
+
+  .githubNotice {
+    border: 1px solid ${({ theme }) => theme.greyColor};
+    padding: 1rem;
+    margin-bottom: 4rem;
+    background-color: #1a1e21;
+    background-color: ${({ theme }) => (theme.current === "dark" ? "1a1e21" : "#f6e7ea")};
+    h3 {
+      font-size: 1.6rem;
+      font-family: "Poppins";
+      text-align: center;
+
+      color: ${({ theme }) => theme.orange};
+    }
+    p {
+      font-size: 1.4rem;
+    }
+  }
+  ${customScrollBar};
+  h1 {
+    text-align: center;
+  }
+  img {
+    filter: none;
+  }
+  blockquote {
+    background-color: ${({ theme }) => theme.bgColor4};
+    padding: 0.5rem 1rem;
+    white-space: pre-wrap;
+  }
+
+  ${media.medium} {
+    overflow: visible;
+  }
+`;
+
+const ReadmeFloatingMenu = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 50%;
+  border: 2px solid ${({ theme }) => theme.pointColor};
+  border-right: 0px;
+  border-bottom: 0px;
+  padding: 0.8rem 1.6rem;
+  border-top-left-radius: 4px;
+`;
+
+interface PortFolioReadMeProps {
+  visible: boolean;
+  data?: RepositoryConstant;
+  onClose: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function PortFolioReadMe({ visible, data = PROJECT_INFOMATION[4], onClose }: PortFolioReadMeProps) {
+  const readmeData = useReadme("colorProject", visible);
+  const wheelStop = useWheelStopPropagation<HTMLDivElement>();
+  const mobileWheel = useWheelStopPropagation<HTMLDivElement>(window.innerWidth < 768);
+  return (
+    <PortFolioReadMeLayout
+      $visible={visible}
+      ref={mobileWheel.ref}
+      onWheel={mobileWheel.onWheel}
+      onTouchStart={mobileWheel.onWheel}
+      onTouchMove={mobileWheel.onWheel}
+      onTouchEnd={mobileWheel.onWheel}
+    >
+      <FirstSection $visible={visible}>
+        <h1>{data.alternateTitle ? data.alternateTitle : data.name}</h1>
+        <h2>What is?</h2>
+        <p>{data.description}</p>
+        <h2>Why?</h2>
+        <p>{data.whyDeveloped}</p>
+        <h2>Functions</h2>
+        <ul className="functions">
+          {data.functions.map((func) => (
+            <li key={func}>{func}</li>
+          ))}
+        </ul>
+        <h2>Stacks</h2>
+        <ul className="stack">
+          {data.stacks &&
+            data.stacks.map((skill) => {
+              return (
+                <li key={skill}>
+                  <img
+                    alt={skill}
+                    src={`https://img.shields.io/badge/${skill.toUpperCase()}-${getSkillColor(
+                      skill
+                    )}?&style=for-the-badge&logo=javascript&logoColor=white`}
+                  />
+                </li>
+              );
+            })}
+        </ul>
+        <h2>Used Library</h2>
+        <ul className="library">{data.library && data.library.map((lib) => <li key={lib}>{lib}</li>)}</ul>
+      </FirstSection>
+      <ReadmeSection
+        as={MarkdownStyled}
+        $visible={visible}
+        ref={wheelStop.ref}
+        onWheel={wheelStop.onWheel}
+        onTouchStart={mobileWheel.onWheel}
+        onTouchMove={mobileWheel.onWheel}
+        onTouchEnd={mobileWheel.onWheel}
+      >
+        <div className="githubNotice">
+          <h3>Notice</h3>
+          <p>하단 내용은 Github의 해당 프로젝트 Repository의 README.md 를 파싱한 내용입니다.</p>
+        </div>
+        {readmeData.isSuccess && !readmeData.isLoading ? (
+          <MarkdownRender
+            markdown={readmeData.data}
+            dirComponent={
+              <DirComponentCss>
+                <MarkdownDirTree projectName={data.name} />
+              </DirComponentCss>
+            }
+          />
+        ) : (
+          <LoadingComponent />
+        )}
+      </ReadmeSection>
+      <ReadmeFloatingMenu>
+        <button type="button" onClick={onClose}>
+          나가기
+        </button>
+      </ReadmeFloatingMenu>
+    </PortFolioReadMeLayout>
+  );
+}
--- src/components/Home/PortFolioSection/PortFolioSlide.tsx
+++ src/components/Home/PortFolioSection/PortFolioSlide.tsx
@@ -1,178 +1,282 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import styled, { css } from 'styled-components'
-import { FloationSide } from 'styles/keyFrame/floating'
-import media from 'styles/media'
-import { Repository } from 'types/Project'
-import PortFolioSlideItem from './PortFolioSlideItem'
-import PortFolioSlideDetails from './PortFolioSlideDetails'
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import styled, { css, keyframes } from "styled-components";
+import { FloationSide } from "styles/keyFrame/floating";
+import media from "styles/media";
+import { Repository } from "types/Project";
+import PortFolioSlideItem from "./PortFolioSlideItem";
+import PortFolioSlideDetails from "./PortFolioSlideDetails";
+import { FadeIn } from "styles/keyFrame/Fade";
+import useSectionAnimation from "../Sections/hooks/useSectionAnimation";
+import { AnimationComponent } from "styles/animation";
+import { useLocation, useNavigate } from "react-router-dom";
+import { ArrowButton } from "components/Common/Buttons/ArrowButtons";
+import PortFolioReadMe from "./PortFolioReadMe";
 
-const PortFolioSlideLayout = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: start;
-    justify-content: center;
-    gap: 2rem;   
-    position: relative;
-    padding-top: calc(var(--header) * 2);
+const PortFolioSlideLayout = styled(AnimationComponent)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  gap: 2rem;
+  position: relative;
+  padding-top: calc(var(--header) * 2);
 
-    .slideBtn {
-        position: absolute;
-        top: calc(50% - 2.4rem);
-        z-index: 4;
-        svg {
-            fill: white;
-            filter: drop-shadow(0px 0px 5px ${({theme})=>theme.shadowColor2});
-            width : 4.8rem;
-            height: 4.8rem;
-        }
+  .slideBtn {
+    position: absolute;
+    top: calc(50% - 2.4rem);
+    z-index: 4;
+    opacity: 0;
+    svg {
+      filter: drop-shadow(0px 0px 5px ${({ theme }) => theme.shadowColor2});
+      width: 4.8rem;
+      height: 4.8rem;
     }
-    .slideBtn.left {
-        left:1.2rem;
-        animation: ${()=>FloationSide(-40)} 2s ease-out infinite;
-    }
-    .slideBtn.right {
+  }
+
+  ${(props) =>
+    props.$enableAnimation &&
+    props.$visible &&
+    css`
+      .slideBtn.left {
+        left: 1.2rem;
+        animation: ${() => FloationSide(-40)} 2s ease-out infinite, ${FadeIn} 1s 1s forwards;
+      }
+      .slideBtn.right {
         right: 1.2rem;
-        animation: ${()=>FloationSide(40)} 2s ease-out infinite;
-    }
+        animation: ${() => FloationSide(40)} 2s ease-out infinite, ${FadeIn} 1s 1s forwards;
+      }
+    `}
 
-    .blur {
-        ${({ theme }) => theme.current === "light" && css`
-            display: none;
-        `}
-        position: absolute;
-        bottom:0;
-        width: 100%;
-        height: 40%;
-        ${({theme})=> theme.current ==="dark" ? css`
-            background: linear-gradient(to bottom, rgba(0, 0, 0,0 ), rgba(0, 0, 0, 1));
+  .blur {
+    ${({ theme }) =>
+      theme.current === "light" &&
+      css`
+        display: none;
+      `}
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 40%;
+    ${({ theme }) =>
+      theme.current === "dark"
+        ? css`
+            background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
             backdrop-filter: blur(1px);
-        
-        `: css`
-            background: linear-gradient(to bottom, rgba(255, 255, 255,0.2 ), rgba(255, 255, 255, 1));
+          `
+        : css`
+            background: linear-gradient(to bottom, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 1));
             backdrop-filter: blur(3px);
-        `}
-        pointer-events: none;
-        z-index: 1;
-    }
+          `}
+    pointer-events: none;
+    z-index: 1;
+  }
 
-    ${media.small}{
-        padding-top : 0;
-        align-items : center;
+  ${media.small} {
+    padding-top: 0;
+    align-items: center;
+  }
+`;
+const SlideWindowAnimation = keyframes`
+    0% {
+        
+        filter : blur(20px);
     }
-`
-const PortFolioSlideWindow = styled.div`
-    position: relative;
-    width: calc(var(--width) / 3.5);
-    height: calc(var(--width) / 3.5 * 1.35);
-    transform-style: preserve-3d;
-    perspective: 1500px;
-    border-radius: 8px;
+    70% {
+        filter : blur(4px);
+    }
+    100% {
+        filter : blur(0px);
+    }
+`;
+
+const PortFolioSlideWindow = styled(AnimationComponent)`
+  position: relative;
+  width: calc(var(--width) / 3.5);
+  height: calc(var(--width) / 3.5 * 1.35);
+  transform-style: preserve-3d;
+  perspective: 1200px;
+  border-radius: 8px;
+  transition: transform 1s ease-out;
+  opacity: 1;
+
+  ${(props) =>
+    !props.$visible &&
+    css`
+      transform: translateZ(-100px) translateY(-200%);
+    `}
+
+  ${(props) =>
+    props.$enableAnimation &&
+    css`
+      /* animation: ${SlideWindowAnimation} 0.5s ease-out forwards; */
+    `}
 
     ${media.xlarge} {
-        width: calc(var(--width) / 3.5);
-        height: calc(var(--width) / 3.5 * 1.35);
-    }
-    ${media.large} {
-        width: calc(var(--width) /2.5);
-        height: calc(var(--width) /2.5 * 1.35);
-    }
-    ${media.medium} {
-        width: calc(var(--width) /2);
-        height: calc(var(--width) /2 * 1.35);
-    }
+    width: calc(var(--width) / 3.5);
+    height: calc(var(--width) / 3.5 * 1.35);
+  }
+  ${media.large} {
+    width: calc(var(--width) / 2.5);
+    height: calc(var(--width) / 2.5 * 1.35);
+  }
+  ${media.medium} {
+    width: calc(var(--width) / 2);
+    height: calc(var(--width) / 2 * 1.35);
+  }
 
-    ${media.small} {
-        width: calc(var(--width) / 1.2);
-        height: calc(var(--width) / 1.2 * 1.35);
-    }
-    
-  
-`
+  ${media.small} {
+    width: calc(var(--width) / 1.8);
+    height: calc(var(--width) / 1.8 * 1.35);
+  }
+`;
 
 interface ProjectSliderProps {
-    slideIndex: number;
-    data: Array<Repository>;
-    setSlide: React.Dispatch<React.SetStateAction<number>>;
+  slideIndex: number;
+  data: Array<Repository>;
+  setSlide: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const BUTTON_DELAY = 570;
 
-
 export default function PortFolioSlide({ slideIndex, data, setSlide }: ProjectSliderProps) {
-    const windowRef = useRef<HTMLDivElement>(null);
-    const [isButtonDelay, setButtonDelay] = useState(false);
+  const windowRef = useRef<HTMLDivElement>(null);
+  const [isButtonDelay, setButtonDelay] = useState(false);
+  const animationState = useSectionAnimation(3);
+  const [isReadMore, setReadMore] = useState<boolean>(false);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (slideIndex < 0 || slideIndex >= data.length) {
-            
-            setTimeout(() => {
-                windowRef.current?.classList.remove("trans");
-                setSlide(slideIndex < 0 ? data.length-1 : 0);
-            }, 500);
-            setTimeout(() => {
-                windowRef.current?.classList.add("trans");
-            }, 600)
-        }
-       
-    }, [slideIndex,data,setSlide])
-    
-    const calculateTranslate = (distance: number) => {
-        let translate = "";
-        if (distance === 0) {
-            translate += " scale(1)";
-            return translate;
-        }
-        if (Math.abs(distance) === 1) {
-            translate += " scale(0.8)";
-            translate += ` rotateY(${distance <0 ? '-' :"+"}20deg)`
-        }
-        else if (Math.abs(distance) === 2) {
-            translate += " scale(0.7)"
-            translate += ` rotateY(${distance <0 ? '-' : "+"}35deg)`
-        }
-        return translate;
+  useEffect(() => {
+    let timerId: NodeJS.Timer;
+    if (location.hash === "#3") setReadMore(false);
+    else {
+      timerId = setTimeout(() => {
+        setReadMore(false);
+      }, 600);
     }
 
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [location.hash]);
 
-    
-    const handlerButtons = useCallback((type: "increase" | "decrease") => {
-        if (isButtonDelay) return;
-        if (type === "increase") {
-            setSlide(prev => prev+1) 
-        }
-        if (type === "decrease") {
-            setSlide(prev => prev-1)    
-        }
-        setButtonDelay(true);
-        setTimeout(() => {
-            setButtonDelay(false);
-        },BUTTON_DELAY)
-    },[isButtonDelay])
+  useEffect(() => {
+    if (slideIndex < 0 || slideIndex >= data.length) {
+      setTimeout(() => {
+        windowRef.current?.classList.remove("trans");
+        setSlide(slideIndex < 0 ? data.length - 1 : 0);
+      }, 500);
+      setTimeout(() => {
+        windowRef.current?.classList.add("trans");
+      }, 600);
+    }
+  }, [slideIndex, data, setSlide]);
+
+  const calculateTranslate = useCallback((distance: number) => {
+    let translate = "";
+    const direction = distance < 0 ? "-" : "+";
+
+    if (distance === 0) {
+      translate += "translateZ(0px)";
+      return translate;
+    }
+    translate += "translateY(10%)";
+    if (Math.abs(distance) === 1) {
+      translate += " scale(0.8)";
+      translate += ` rotateY(${direction}15deg)`;
+    } else if (Math.abs(distance) === 2) {
+      translate += " scale(0.7)";
+      translate += ` rotateY(${direction}30deg)`;
+    } else if (Math.abs(distance) >= 3) {
+      translate += " scale(0.6)";
+      translate += ` rotateY(${direction}45deg)`;
+    }
+    return translate;
+  }, []);
+
+  const onToggleIsMore = useCallback<React.MouseEventHandler>(() => {
+    setReadMore((prev) => !prev);
+  }, []);
+
+  const handlerButtons = useCallback(
+    (type: "increase" | "decrease") => {
+      if (isButtonDelay) return;
+      if (type === "increase") {
+        setSlide((prev) => prev + 1);
+      }
+      if (type === "decrease") {
+        setSlide((prev) => prev - 1);
+      }
+      setButtonDelay(true);
+      setTimeout(() => {
+        setButtonDelay(false);
+      }, BUTTON_DELAY);
+    },
+    [isButtonDelay]
+  );
 
   return (
-      <PortFolioSlideLayout>
-          <PortFolioSlideWindow className="trans" ref={windowRef}>
-          {data.map((item, idx) => {
-              return <PortFolioSlideItem data={item} key={item.id + "prev"} distance={slideIndex - idx + data.length} trans={calculateTranslate(slideIndex - idx + data.length)}/>
-            })}
-            {data.map((item, idx) => {
-                return <PortFolioSlideItem data={item} key={item.id } distance={slideIndex - idx} trans={calculateTranslate(slideIndex-idx)} />
-            })}
-            {data.map((item, idx) => {
-                return <PortFolioSlideItem data={item} key={item.id + "next"} distance={slideIndex - idx - data.length} trans={calculateTranslate(slideIndex - idx - data.length)}/>
-            })}
-          </PortFolioSlideWindow>
-          <PortFolioSlideDetails key={Math.abs(slideIndex)} project={data[Math.abs(slideIndex) % data.length]}/>
-          <div className="blur"></div>
-          <button type='button' className="slideBtn left"  onClick={()=>{
-              handlerButtons("decrease");
-          }}>
-              <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591 1.299-1.002 3.945-3.044 5.498-4.243z" /></svg>
-          </button>
-          <button type='button' className="slideBtn right" onClick={()=>{
-            handlerButtons("increase");
-          }}><svg clipRule="evenodd"  fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"/></svg></button>
+    <PortFolioSlideLayout $enableAnimation={animationState === "animation-active"} $visible={!isReadMore}>
+      <PortFolioSlideWindow
+        className={`trans`}
+        ref={windowRef}
+        $visible={!isReadMore}
+        $enableAnimation={animationState === "animation-active"}
+      >
+        {data.map((item, idx) => {
+          return (
+            <PortFolioSlideItem
+              data={item}
+              key={item.id + "prev"}
+              distance={slideIndex - idx + data.length}
+              trans={calculateTranslate(slideIndex - idx + data.length)}
+            />
+          );
+        })}
+        {data.map((item, idx) => {
+          return (
+            <PortFolioSlideItem
+              data={item}
+              key={item.id}
+              distance={slideIndex - idx}
+              trans={calculateTranslate(slideIndex - idx)}
+            />
+          );
+        })}
+        {data.map((item, idx) => {
+          return (
+            <PortFolioSlideItem
+              data={item}
+              key={item.id + "next"}
+              distance={slideIndex - idx - data.length}
+              trans={calculateTranslate(slideIndex - idx - data.length)}
+            />
+          );
+        })}
+      </PortFolioSlideWindow>
+      <PortFolioSlideDetails
+        visible={!isReadMore}
+        key={Math.abs(slideIndex)}
+        project={data[Math.abs(slideIndex) % data.length]}
+        onClickButton={onToggleIsMore}
+      />
+      {/* <div className="blur" /> */}
+      <ArrowButton
+        className="slideBtn left"
+        direction="left"
+        onClick={() => {
+          handlerButtons("decrease");
+        }}
+      />
+      <ArrowButton
+        direction="right"
+        className="slideBtn right"
+        onClick={() => {
+          handlerButtons("increase");
+        }}
+      />
+      {animationState === "animation-active" && <PortFolioReadMe visible={isReadMore} onClose={onToggleIsMore} />}
     </PortFolioSlideLayout>
-  )
+  );
 }
