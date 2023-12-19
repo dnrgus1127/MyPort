9a49c7cb74b1aef2a14746fbee69559027852094
dnrgus1127 <dnrgus1127@naver.com>
2023-12-19 17:30:20 +0900

    FEAT (스택 ) : 스킬 내용 추가, RestSkills 컴포넌트 추가

--- src/components/Home/Sections/Skills.tsx
+++ src/components/Home/Sections/Skills.tsx
@@ -1,48 +1,66 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import styled, { css } from 'styled-components'
-import textShadow from 'styles/shadow'
-import { SKILL_DESCRIPTION, SKILL_LIST, Skill } from 'constans/SkillsData'
-import media from 'styles/media'
-import { Floating } from 'styles/keyFrame/floating'
-import GlitterStars from 'components/Common/EffectElement/GlitterStars'
-import MeteorEffect from 'components/Common/EffectElement/MeteorEffect'
-import { Rotating } from 'styles/keyFrame/Rotating'
-import SKillHome from '../SkillSection/SKillHome'
-import SkillDescription from '../SkillSection/SkillDescription'
+import GlitterStars from "components/Common/EffectElement/GlitterStars";
+import MeteorEffect from "components/Common/EffectElement/MeteorEffect";
+import { SKILL_LIST, Skill } from "constans/SkillsData";
+import { useCallback, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import RestSkills from "../SkillSection/RestSkills";
+import SKillHome from "../SkillSection/SKillHome";
+import SkillDescription from "../SkillSection/SkillDescription";
 
 const SkillsLayout = styled.div`
-  /* background-color: ${({ theme }) => theme.bgColor4}; */
   height: calc(var(--vh) * 100);
   position: relative;
-  transform-style : preserve-3d;
+  transform-style: preserve-3d;
   perspective: 200px;
-`
+`;
 
-
-export enum SKILLS_NUMBER { Home,Description };
-
+export enum SKILLS_NUMBER {
+  Home,
+  Description,
+  Rest,
+}
 
 export default function Skills() {
   const [selectedSkill, setSelectedSkill] = useState<Skill>(SKILL_LIST[0][2]);
   const [currentPage, setCurrentPage] = useState<SKILLS_NUMBER>(SKILLS_NUMBER.Home);
+  const location = useLocation();
 
-  const setSkillHandler = useCallback((skill : Skill) => {
+  useEffect(() => {
+    if (location.hash === "#2") setCurrentPage(SKILLS_NUMBER.Home);
+  }, [location.hash]);
+
+  const setSkillHandler = useCallback(
+    (skill: Skill) => {
       setSelectedSkill(skill);
-      setCurrentPage(SKILLS_NUMBER.Description); 
-  }, [setCurrentPage, setSelectedSkill])
+      setCurrentPage(SKILLS_NUMBER.Description);
+    },
+    [setCurrentPage, setSelectedSkill]
+  );
 
   const gotoHomePage = useCallback(() => {
     setCurrentPage(SKILLS_NUMBER.Home);
-  }, [setCurrentPage])
+  }, [setCurrentPage]);
 
-
+  const gotoRestPage = useCallback(() => {
+    setCurrentPage(SKILLS_NUMBER.Rest);
+  }, []);
 
   return (
-    <SkillsLayout >
-      <GlitterStars count={15}/>
+    <SkillsLayout>
+      <GlitterStars count={15} />
       <MeteorEffect count={8} minSpeed={5} maxDelay={20} white={false} />
-      <SKillHome visible={currentPage === SKILLS_NUMBER.Home} onClickLogo={setSkillHandler} />
-      <SkillDescription visible={currentPage === SKILLS_NUMBER.Description} currentSkill={selectedSkill} onClickBack={gotoHomePage}/>
+      <SKillHome
+        visible={currentPage === SKILLS_NUMBER.Home}
+        onClickLogo={setSkillHandler}
+        onClickRest={gotoRestPage}
+      />
+      <SkillDescription
+        visible={currentPage === SKILLS_NUMBER.Description}
+        currentSkill={selectedSkill}
+        onClickBack={gotoHomePage}
+      />
+      <RestSkills visible={currentPage === SKILLS_NUMBER.Rest} skillList={SKILL_LIST[1]} />
     </SkillsLayout>
-  )
+  );
 }
