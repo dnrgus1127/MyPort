69beb2d48965de35a537b91f55e43d464a4648ec
dnrgus1127 <dnrgus1127@naver.com>
2023-12-19 17:35:29 +0900

    Style : 코드 포맷팅 수정

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,69 +1,61 @@
-import './App.css';
-import {Outlet, ScrollRestoration, useLocation, useNavigation} from "react-router-dom"
-import PageAnimation from 'components/Common/PageAnimation';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from 'styles/GlobalStyles';
-import { darkTheme, ligthTheme } from 'styles/theme';
-import { useAppSelector } from 'redux/hooks';
-import Header from 'components/Main/Header';
-import Home from 'pages/Home';
-import { QueryClient } from '@tanstack/react-query';
-import { GITHUBAPIKEY } from 'apiKey';
-import { Repository, RepositoryData } from 'types/Project';
-import { PROJECT_INFOMATION } from 'constans/ProjectData';
-import { REPOSITORYS } from 'constans/Config';
-
+import { QueryClient } from "@tanstack/react-query";
+import { GITHUBAPIKEY } from "apiKey";
+import PageAnimation from "components/Common/PageAnimation";
+import Header from "components/Main/Header";
+import { REPOSITORYS } from "constans/Config";
+import { GITHUB403 } from "constans/ErrorMessage";
+import { PROJECT_INFOMATION } from "constans/ProjectData";
+import Home from "pages/Home";
+import { Outlet, json } from "react-router-dom";
+import { Repository, RepositoryData } from "types/Project";
+import { ErrorJson } from "types/errorJson";
+import "./App.css";
 
 export const projectQuery = () => ({
-  queryKey: ['git', "repositoryList"],
+  queryKey: ["git", "repositoryList"],
   queryFn: async () => {
-      const res = await fetch(`https://api.github.com/users/dnrgus1127/repos`, {
-          headers: {
-              Authorization : GITHUBAPIKEY
-          }
-      })
-      if (res.status === 403) {
-          throw new Response("Not Found", { status: 403 });
-      }
-      
+    const res = await fetch(`https://api.github.com/users/dnrgus1127/repos`, {
+      headers: {
+        Authorization: GITHUBAPIKEY,
+      },
+    });
 
-      const data = await res.json();
-      return data ;
+    if (res.status === 403) {
+      let errorBody: ErrorJson = { msg: GITHUB403, sorry: "GitHUB API limit" };
+      throw json(errorBody, { status: 403 });
+    }
+
+    const data = await res.json();
+    return data;
   },
   select: (data: Array<RepositoryData>) => {
-    const repoList = data.filter((repo : RepositoryData) => REPOSITORYS.includes(repo.name));
-  
-        const CompletelyRepository: Array<Repository> = repoList.map((item) => {
-            let idx = PROJECT_INFOMATION.findIndex(repo => repo.name === item.name);
-            return {...item, ...PROJECT_INFOMATION[idx]}
-        })
+    const repoList = data.filter((repo: RepositoryData) => REPOSITORYS.includes(repo.name));
 
-        return CompletelyRepository;
+    const CompletelyRepository: Array<Repository> = repoList.map((item) => {
+      let idx = PROJECT_INFOMATION.findIndex((repo) => repo.name === item.name);
+      return { ...item, ...PROJECT_INFOMATION[idx] };
+    });
+
+    return CompletelyRepository;
   },
 
   staleTime: 60 * 10 * 1000,
-})
+});
 
-export const loader = (queryClient : QueryClient) =>async () => {
+export const loader = (queryClient: QueryClient) => async () => {
   const query = projectQuery();
-  
-  return (
-    queryClient.getQueryData(query.queryKey) ?? (await queryClient.fetchQuery(query))
-  )
-}
 
+  return queryClient.getQueryData(query.queryKey) ?? (await queryClient.fetchQuery(query));
+};
 
 function HomePage() {
-  
   return (
-      <>
-          <Home />
-          <Header />
-          <PageAnimation/>
-          <Outlet />
-      </>
-
-
+    <>
+      <Home />
+      <Header />
+      <PageAnimation />
+      <Outlet />
+    </>
   );
 }
 
