4529b6baecf0aabe9e08b0baae72e6cd4580d38a
dnrgus1127 <dnrgus1127@naver.com>
2023-12-23 18:22:14 +0900

    FEAT : APIKEY 추가

--- src/components/Projects/hooks/useReadme.tsx
+++ src/components/Projects/hooks/useReadme.tsx
@@ -5,7 +5,11 @@ export default function useReadme(projectName: string, enable?: boolean) {
   const readmeData = useQuery<string, Error>({
     queryKey: ["readme", projectName],
     queryFn: async () => {
-      const res = await fetch(`https://raw.githubusercontent.com/dnrgus1127/${projectName}/main/README.md`);
+      const res = await fetch(`https://raw.githubusercontent.com/dnrgus1127/${projectName}/main/README.md`, {
+        headers: {
+          Authorization: GITHUBAPIKEY,
+        },
+      });
       const data = res.text();
       return data;
     },
