733b4319b0abfbc29e12951210e7b4aa281bc7bb
dnrgus1127 <dnrgus1127@naver.com>
2023-12-23 18:06:01 +0900

    FILE : 파일구조 수정

---
+++ src/components/Home/IntroSection/CoverTitle.tsx
@@ -0,0 +1,57 @@
+import useMorphing from "hooks/text/useMorphing";
+import React from "react";
+import styled from "styled-components";
+import media from "styles/media";
+
+const MorphTitle = styled.div`
+  filter: url(#threshold) blur(0.5px);
+
+  // Safari 에만
+  @media not all and (min-resolution: 0.001dpcm) {
+    @supports (-webkit-appearance: none) {
+      filter: url(#threshold);
+    }
+  }
+
+  width: 100%;
+  height: 20%;
+  display: flex;
+  align-items: center;
+
+  span {
+    font-family: "Raleway", sans-serif;
+    font-size: 8rem;
+    position: absolute;
+    display: inline-block;
+    width: 100%;
+    text-align: center;
+    letter-spacing: 5px;
+  }
+  svg {
+    position: absolute;
+  }
+  ${media.medium} {
+    span {
+      font-size: 6rem;
+    }
+  }
+  ${media.small} {
+    span {
+      font-size: 4rem;
+    }
+  }
+`;
+
+const texts = ["FrontEnd", "Programmer", "WookHyun"];
+
+export default function CoverTitle({ className }: { className?: string }) {
+  const { textRef1, textRef2, filter } = useMorphing({ texts });
+
+  return (
+    <MorphTitle className={className}>
+      <span ref={textRef1}></span>
+      <span ref={textRef2}></span>
+      {filter}
+    </MorphTitle>
+  );
+}
