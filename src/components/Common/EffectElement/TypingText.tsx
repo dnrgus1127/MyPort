0e4f50aab2c54ff368f30d2eaaef15dc08ccfd96
dnrgus1127 <dnrgus1127@naver.com>
2023-12-23 18:19:35 +0900

    Chore : 프로퍼티 추가

--- src/components/Common/EffectElement/TypingText.tsx
+++ src/components/Common/EffectElement/TypingText.tsx
@@ -1,45 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 interface TypingTextProps {
-    text: string;
-    tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "li";
-    delay?: number;
-    speed?: number;
-    className?: string;
+  text: string;
+  tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "li";
+  delay?: number;
+  speed?: number;
+  className?: string;
 }
 
-const TypingTextCss = styled.h1`
-    
-`
+const TypingTextCss = styled.h1``;
 
-export default function TypingText({ tag = "h1", text ,delay = 0,speed=100}: TypingTextProps) {
-    const [innerText, setInnerText] = useState<string>("");
+export default function TypingText({ tag = "h1", text, delay = 0, speed = 100, className }: TypingTextProps) {
+  const [innerText, setInnerText] = useState<string>("");
 
-    useEffect(() => {
-        const initDelay = delay;
-        let speed = 100;
-        let length = 1;
-        let typingIntervalId: NodeJS.Timer;
-        // 시작 딜레이 타이머
-        let timerId = setTimeout(() => {
-            typingIntervalId = setInterval(() => {
-                setInnerText(text.substring(0, length));
-                length++;
-                if (length > text.length) {
-                    clearInterval(typingIntervalId);
-                }
-            },speed)
-            return () => {
-                clearInterval(typingIntervalId);
-            }
-        },initDelay)
-
-        return () => {
-            clearTimeout(timerId);
+  useEffect(() => {
+    const initDelay = delay;
+    let speed = 100;
+    let length = 1;
+    let typingIntervalId: NodeJS.Timer;
+    // 시작 딜레이 타이머
+    let timerId = setTimeout(() => {
+      typingIntervalId = setInterval(() => {
+        setInnerText(text.substring(0, length));
+        length++;
+        if (length > text.length) {
+          clearInterval(typingIntervalId);
         }
-    },[text])
+      }, speed);
+      return () => {
+        clearInterval(typingIntervalId);
+      };
+    }, initDelay);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
   return (
-      <TypingTextCss as={tag}>{innerText}</TypingTextCss>
-  )
+    <TypingTextCss className={className} as={tag}>
+      {innerText}
+    </TypingTextCss>
+  );
 }
