6bbc2909bfd056f24fe451689d455d3e594e132c
dnrgus1127 <dnrgus1127@naver.com>
2023-12-23 18:24:29 +0900

    FEAT,style : APIKEY 추가, 코드 포맷팅 수정

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,42 +1,40 @@
-import { QueryClient, useQuery } from '@tanstack/react-query';
-import PostLayout from 'components/Blog/Post/PostLayout';
-import React from 'react'
-import { Params, useParams } from 'react-router-dom'
-import { checkStatus } from 'utils/fetch/checkStatus';
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { GITHUBAPIKEY } from "apiKey";
+import PostLayout from "components/Blog/Post/PostLayout";
+import { Params, useParams } from "react-router-dom";
+import { checkStatus } from "utils/fetch/checkStatus";
 
+const githubMarkdownQuery = (fileName: string) => ({
+  queryKey: ["git", "md", fileName],
+  queryFn: async () => {
+    // const res = await fetch(`https://raw.githubusercontent.com/dnrgus1127/TIL/main/${fileName.split("/").map(item => encodeURIComponent(item)).join("/")}`)
+    const res = await fetch(`https://raw.githubusercontent.com/dnrgus1127/TIL/main/${encodeURIComponent(fileName)}`, {
+      headers: {
+        Authorization: GITHUBAPIKEY,
+      },
+    });
+    checkStatus(res.status);
 
-const githubMarkdownQuery = (fileName : string) => ({
-    queryKey: ["git", "md", fileName],
-    queryFn: async () => {
-        // const res = await fetch(`https://raw.githubusercontent.com/dnrgus1127/TIL/main/${fileName.split("/").map(item => encodeURIComponent(item)).join("/")}`)
-        const res = await fetch(`https://raw.githubusercontent.com/dnrgus1127/TIL/main/${encodeURIComponent(fileName)}`)
-        checkStatus(res.status);
-      
-        const data = res.text();
-        return data;
-    },
-    staleTime : 60 * 60 * 1000,
-})
+    const data = res.text();
+    return data;
+  },
+  staleTime: 60 * 60 * 1000,
+});
 
-export const loader = (queryClient: QueryClient) => async ({ params } : {params : Params<string>}) => {
-    
+export const loader =
+  (queryClient: QueryClient) =>
+  async ({ params }: { params: Params<string> }) => {
     if (!params["*"]) throw new Response("route Error", { status: 404 });
     const query = githubMarkdownQuery(params["*"]);
-    
 
-    return (
-        await queryClient.fetchQuery(query)
-    )
-    
-}
+    return await queryClient.fetchQuery(query);
+  };
 
 export default function PostPage() {
-   const params = useParams();
-   const { data: post, isSuccess } = useQuery(githubMarkdownQuery(params["*"]!))
-    
-   if (!isSuccess) return <></>;
-    
-  return (
-    <PostLayout title={params["*"]!} post={post} />
-  )
+  const params = useParams();
+  const { data: post, isSuccess, isLoading } = useQuery(githubMarkdownQuery(params["*"]!));
+
+  if (!isSuccess) return <></>;
+
+  return <PostLayout title={params["*"]!} post={post} />;
 }
