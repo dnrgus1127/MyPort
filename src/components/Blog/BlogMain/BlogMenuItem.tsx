ab981b832dd4ceab684ad9280b87d1a35faa7ce3
dnrgus1127 <dnrgus1127@naver.com>
2023-12-28 15:49:11 +0900

    Style : 코드 포맷팅 수정

--- src/components/Blog/BlogMain/BlogMenuItem.tsx
+++ src/components/Blog/BlogMain/BlogMenuItem.tsx
@@ -1,73 +1,74 @@
-import { BoxButton } from 'components/Common/Buttons/StyledButtons'
-import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
-import styled, { css } from 'styled-components'
-import media from 'styles/media';
+import { BoxButton } from "components/Common/Buttons/StyledButtons";
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled, { css } from "styled-components";
+import media from "styles/media";
 
+const MenuItem = styled(BoxButton)<{ $clicked: boolean }>`
+  font-family: "SUIT-Regular";
 
-const MenuItem = styled(BoxButton)<{$clicked : boolean}>`
-  font-family: 'SUIT-Regular';
-  
   cursor: pointer;
-  font-size: 1.5rem;
-  padding : .8rem 2.2rem;
-  transition: .1s all ease-out;
-  font-style : italic;
+  font-size: 1.6rem;
+  padding: 0.8rem 2.2rem;
+  transition: 0.1s all ease-out;
 
-  ${(props)=> props.$clicked && css`
-    background-color: ${({theme})=> theme.bgColor};
-    color : ${({ theme }) => theme.color};
-    position: relative;
-    &::after {
-        content:  "x";
+  ${(props) =>
+    props.$clicked &&
+    css`
+      background-color: ${({ theme }) => theme.bgColor};
+      color: ${({ theme }) => theme.color};
+      position: relative;
+      font-style: italic;
+
+      &::after {
+        content: "x";
         font-style: initial;
         text-align: center;
         line-height: 1.3rem;
         font-size: 1.6rem;
         position: absolute;
-        top: -.8rem;
-        right: -.8rem;
+        top: -0.8rem;
+        right: -0.8rem;
         width: 1.6rem;
         height: 1.6rem;
         border-radius: 50%;
-        color : ${({theme})=>theme.bgColor};
+        color: ${({ theme }) => theme.bgColor};
         border: 1px solid ${({ theme }) => theme.color};
         background-color: ${({ theme }) => theme.color2};
-  
-    }
-  
-  `}
+      }
+    `}
 
-  ${media.small}{
-    padding : .5rem 2rem;
+  ${media.small} {
+    padding: 0.5rem 2rem;
     font-size: 1.4rem;
   }
-`
-
+`;
 
 interface BlogMenuItemProps {
-    topic: string;
+  topic: string;
 }
 
 export default function BlogMenuItem({ topic }: BlogMenuItemProps) {
-    const navigate = useNavigate();
-    const params = useParams();
+  const navigate = useNavigate();
+  const params = useParams();
 
-    
   return (
-      <MenuItem as="li" $clicked={params.category ? true : false} onClick={() => {
-          if (params.category) {
-              navigate("/blog/main");
+    <MenuItem
+      as="li"
+      $clicked={params.category ? true : false}
+      onClick={() => {
+        if (params.category) {
+          navigate("/blog/main");
+        } else {
+          if (topic === "Latest") {
+            navigate("/blog/main/Latest");
+          } else {
+            navigate(`/blog/main/${topic}`);
           }
-          else {
-            if (topic === "Latest") {
-                navigate("/blog/main/Latest");
-            }
-            else {
-                navigate(`/blog/main/${topic}`);
-            }    
-          }
-          
-    }}>{topic}</MenuItem>
-  )
+        }
+      }}
+    >
+      {topic}
+    </MenuItem>
+  );
 }
--- src/components/Blog/BlogMain/PostsMenu.tsx
+++ src/components/Blog/BlogMain/PostsMenu.tsx
@@ -1,36 +1,31 @@
-
-import React from 'react'
-import {  useParams } from 'react-router-dom'
-import styled from 'styled-components'
-import { FadeIn } from 'styles/keyFrame/Fade'
-import BlogMenuItem from './BlogMenuItem'
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { FadeIn } from "styles/keyFrame/Fade";
+import BlogMenuItem from "./BlogMenuItem";
 
 const MenuContainer = styled.div`
   max-width: var(--width);
   margin: 0 auto;
 
-
   h2 {
-    font-family: "Roboto KR","Noto Sans KR", sans-serif;
+    font-family: "Roboto KR", "Noto Sans KR", sans-serif;
     font-weight: 800;
     margin-bottom: 1em;
     font-size: 1.5rem;
   }
-`
+`;
 
 const MenuBox = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  padding-bottom: 2rem;
+  border-bottom: 2px solid ${({ theme }) => theme.color};
+  font-weight: 600;
 
-    display: flex;
-    flex-wrap: wrap;
-    gap: .5rem;
-    padding-bottom: 2rem;
-    border-bottom: 2px solid ${({theme})=> theme.color};
-
-    margin-bottom:var(--side-padding);
-`
-
-
+  margin-bottom: var(--side-padding);
+`;
 
 interface BlogMenusProps {
   topics: Array<string>;
@@ -40,12 +35,15 @@ export default function BlogMenus({ topics }: BlogMenusProps) {
   const params = useParams();
   return (
     <MenuContainer>
-      <h2>Topics</h2> 
+      <h2>Topics</h2>
       <MenuBox>
-        {!params.category && <BlogMenuItem key="Latest" topic='Latest' />}
-        {params.category ? <BlogMenuItem key={params.category} topic={params.category}/> : topics.map(topic => <BlogMenuItem key={topic} topic={topic}/>)}
+        {!params.category && <BlogMenuItem key="Latest" topic="Latest" />}
+        {params.category ? (
+          <BlogMenuItem key={params.category} topic={params.category} />
+        ) : (
+          topics.map((topic) => <BlogMenuItem key={topic} topic={topic} />)
+        )}
       </MenuBox>
-      
     </MenuContainer>
-  )
+  );
 }
