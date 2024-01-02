166eccd74b0886a3667dbd87a893c7c04869188d
dnrgus1127 <dnrgus1127@naver.com>
2024-01-02 18:39:35 +0900

    FIX : 포스트 제목 물음표 uri encode 적용

--- src/components/Home/BlogSection/PostSection.tsx
+++ src/components/Home/BlogSection/PostSection.tsx
@@ -1,6 +1,6 @@
 import { Tree } from "components/Common/Markdown/types/tree";
-import React, { useLayoutEffect, useMemo } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import media from "styles/media";
 
@@ -21,20 +21,9 @@ const PostSectionLayout = styled.div`
     text-decoration: underline;
   }
 
-  &:nth-child(2) {
-    text-align: end;
-    h3::after {
-      left: -20%;
-    }
-  }
-
   ${media.large} {
     width: 100%;
     padding: 4rem 0;
-
-    h3 {
-      font-size: 2.4rem;
-    }
     li {
       font-size: 1.8rem;
 
@@ -50,8 +39,6 @@ interface PostSectionProps {
 }
 
 export default function PostSection({ postList, topics, count = 4 }: PostSectionProps) {
-  const navigate = useNavigate();
-
   let filteredPostList = useMemo(() => {
     let result;
     if (topics === "Latest Posts") {
@@ -65,7 +52,6 @@ export default function PostSection({ postList, topics, count = 4 }: PostSection
 
   return (
     <PostSectionLayout>
-      <h2>{topics}</h2>
       <ul>
         {filteredPostList.map((post, idx) => {
           if (idx >= count) return;
@@ -73,7 +59,7 @@ export default function PostSection({ postList, topics, count = 4 }: PostSection
           let title = tokens[tokens.length - 1];
           return (
             <li key={idx}>
-              <Link to={`/blog/post/${post.path}`}>{title.substring(0, title.length - 3)}</Link>
+              <Link to={`/blog/post/${encodeURIComponent(post.path)}`}>{title.substring(0, title.length - 3)}</Link>
             </li>
           );
         })}
