d7c264948a032529182e373df8c60b27da4aa1ef
dnrgus1127 <dnrgus1127@naver.com>
2024-01-02 18:41:13 +0900

    FEAT : 포트폴리오 데이터 수정 및 레이아웃 조정

--- src/types/Project.tsx
+++ src/types/Project.tsx
@@ -1,19 +1,19 @@
 export interface RepositoryData {
-    repo: string;
-    name: string;
-    id: number;
+  repo: string;
+  name: string;
+  id: number;
 }
 
 export interface RepositoryConstant {
-    name: string;
-    alternateTitle?: string;
-    stacks?: Array<string>;
-    description: string;
-    whyDeveloped: string;
-    functions: Array<string>;
-    library?: Array<string>; 
-    review?: string;
+  name: string;
+  alternateTitle?: string;
+  stacks?: Array<string>;
+  description: string;
+  whyDeveloped: string;
+  preview: string;
+  functions: Array<string>;
+  library?: Array<string>;
+  review?: string;
 }
 
-export type Repository = RepositoryData & RepositoryConstant 
-
+export type Repository = RepositoryData & RepositoryConstant;
